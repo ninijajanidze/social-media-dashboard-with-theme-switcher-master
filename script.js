87c8638d60fdadb1d42c07df9034602a9ea20d55
nininini <jajanidzenino60@gmail.com>
2024-03-11 23:56:01 +0400

    first commit

---
+++ script.js
@@ -0,0 +1,8 @@
+
+const darkBtn = document.querySelector(".dark-mode");
+const darkMode = document.querySelector(".dark");
+
+darkBtn.addEventListener("click", () => {
+  darkMode.classList.toggle("dark")
+  
+});
